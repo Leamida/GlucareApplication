b9f45a391d6c88fe1602cba06c0330478e037297
leamida <leoanangmh@gmail.com>
2022-06-06 16:36:30 +0700

    fix sign in with google

--- app/src/main/java/com/example/glucareapplication/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/example/glucareapplication/ui/auth/AuthActivity.kt
@@ -53,7 +53,7 @@ class AuthActivity : AppCompatActivity() {
                     // Your server's client ID, not your Android client ID.
                     .setServerClientId(getString(R.string.your_web_client_id))
                     // Only show accounts previously used to sign in.
-                    .setFilterByAuthorizedAccounts(true)
+                    .setFilterByAuthorizedAccounts(false)
                     .build()
             )
             .build()
